43c9836c25bab5faea63d04a566a6d3317d5d51b
Renan Maia <renansoa64@gmail.com>
2024-11-18 14:31:43 -0300

    Add files via upload

---
+++ Gamescard/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Sega',
+    'Em que ano foi lançado o jogo Sonic The Hedgehog de Mega Drive?',
+    'O Jogo Sonic The Hedgehog foi lançado no ano de 1991'
+)
+
+criaCartao(
+    'Microsoft',
+    'Qual é o nome do personagem que veste uma armadura verde e que hoje em dia é "mascote" da Xbox?',
+    'O nome do personagem é MasterChief da serie de jogos da Xbox, Halo'
+)
+
+criaCartao(
+    'Sony',
+    'O que você precisa para fazer o final bom do jogo Crash Bandicoot de PlayStation 1?',
+    'Você precisa pegar todas as gemas de todas as fases do jogo, para pegar elas, você precisa pegar todas as caixas das fases'
+)
+
+criaCartao(
+    'Nintendo',
+    'Qual foi o Primeiro jogo com a aparição do Mario?',
+    'Ele apareceu pela primeira vez em um jogo de fliperama, o nome era "Mario Bros."'
+)
